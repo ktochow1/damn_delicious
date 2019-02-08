cc06259bc1cf48a77108e24b714096de700e48eb
Kat Tochowicz <ktochow1@binghamton.edu>
2019-02-07 23:18:51 -0500

    got 2nd level (kind of) working

--- lib/maangchis_cookbook/scraper.rb
+++ lib/maangchis_cookbook/scraper.rb
@@ -3,7 +3,10 @@ class Scraper
   def self.scrape_categories(url)
     website = Nokogiri::HTML(open(url))
     section = website.css("div.itemized")
-    section.css("div a.attachment-thumbnail.size-thumbnail.wp-post-image").text
+    section.css("div #post-28457")
+
+#     pry(Scraper)> section.css("div #post-28457")[0].text
+# => "\n\n\nBeef short ribs soup\n\nGalbitang\t\n갈비탕 "
     #^gives me an empty array
     binding.pry
 
