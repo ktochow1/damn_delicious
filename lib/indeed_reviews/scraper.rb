e17dd6cf0d0e726e4be5d1293da25784348b200d
Kat Tochowicz <ktochow1@binghamton.edu>
2019-02-19 22:55:03 -0500

    polished take 2

--- lib/indeed_reviews/scraper.rb
+++ lib/indeed_reviews/scraper.rb
@@ -4,8 +4,7 @@ class Scraper
     homepage = Nokogiri::HTML(open(url))
     job_container = homepage.css("div.cmp-SearchResultContainer div.cmp-CompanyListWidget")#[0]
     job_strip_array = job_container.css(".clearfix.cmp-CompanyWidget")
-    # review_link = job_strip_array.css("div.cmp-CompanyWidget-links")[0].children[0].attributes["href"].value # job_container.css("a")[0].attributes["href"].value
-    #
+    
      job_strip_array.each.with_index(1) do |job_row, index|
         job_name = job_row.css(".cmp-CompanyWidget-details a.cmp-CompanyWidget-name").text
         job_url = job_row.css(".cmp-CompanyWidget-links a")[0].attributes["href"].value
